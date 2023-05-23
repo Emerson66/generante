4528310c9472792da648de27e92f2c7f3a8f3133
emerson66 <emersonreisdacosta@gmail.com>
2023-05-23 16:01:42 -0400

    Projeto Criado

---
+++ src/main/java/br/com/buritiscript/generante/GeneranteApplication.java
@@ -0,0 +1,13 @@
+package br.com.buritiscript.generante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/buritiscript/generante/GeneranteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.buritiscript.generante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
