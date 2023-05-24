e083d3c001ebcd2e43764eb97d376c2e9e126db7
Emerson Reis da Costa <emerson.costa@pmbv.local>
2023-05-24 17:15:28 -0400

    consertado listUploadedFiles() agora ele retorna uma lista em vez de redirecionar para uma model

--- src/main/java/br/com/buritiscript/generante/storage/controller/FileUploadController.java
+++ src/main/java/br/com/buritiscript/generante/storage/controller/FileUploadController.java
@@ -1,12 +1,12 @@
 package br.com.buritiscript.generante.storage.controller;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,9 +14,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 import br.com.buritiscript.generante.storage.service.StorageService;
+
 
 @RestController
 public class FileUploadController {
@@ -29,14 +30,14 @@ public class FileUploadController {
     }
 
     @GetMapping("/")
-	public String listUploadedFiles(Model model) throws IOException {
+	public List<?> listUploadedFiles() throws IOException {
 
-		model.addAttribute("files", storageService.loadAll().map(
+		return storageService.loadAll().map(
 				path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
 						"serveFile", path.getFileName().toString()).build().toUri().toString())
-				.collect(Collectors.toList()));
+				.collect(Collectors.toList());
 
-		return "uploadForm";
+		 
 	}
     
     @GetMapping("/files/{filename:.+}")
